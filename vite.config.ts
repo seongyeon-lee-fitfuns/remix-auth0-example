a01f11e70c4f825647b4a45b4deafd76251daef9
seongyeon-lee-fitfuns <sy.lee@fitfuns.com>
2025-04-17 21:23:10 +0900

    vite.config.ts 파일 수정: 빌드 시 react-dom/server의 별칭을 설정하는 resolve 옵션 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+  resolve: {
+    alias: command === "build" 
+        ? {
+            "react-dom/server": "react-dom/server.node",
+          }
+        : undefined,
+  },
+}));
